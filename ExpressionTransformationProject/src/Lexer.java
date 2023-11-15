7710f5a3c7594427a9d70706521b53e474150619
Andres Morilla <andresmm4242@gmail.com>
2023-11-15 20:31:15 +0100

    create final project

---
+++ ExpressionTransformationProject/src/Lexer.java
@@ -0,0 +1,2 @@
+public class Lexer {
+}
--- ExpressionTransformationProject/src/Main.java
+++ ExpressionTransformationProject/src/Main.java
@@ -1,11 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.println("usted gracias");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        try(Scanner sc = new Scanner(new File("test.txt"))){
+
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+        }
+
+
 
 
     }
