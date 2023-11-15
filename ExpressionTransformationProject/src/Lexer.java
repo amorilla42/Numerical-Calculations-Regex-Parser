b00ec87b9da3fdf55b025b5f7b549202f97eb88f
Andres Morilla <andresmm4242@gmail.com>
2023-11-15 21:47:56 +0100

    Lexer first iteration

--- ExpressionTransformationProject/src/Lexer.java
+++ ExpressionTransformationProject/src/Lexer.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer {
+
+    public List<String> tokenizer(String line){
+        String[] split = line.split("[() ,]+");
+
+        List<String> tokens = new ArrayList<>();
+
+        for(String s : split)
+        {
+            switch (s.toLowerCase()){
+                case "add": tokens.add(" + ");
+                            break;
+                case "sub": tokens.add(" - ");
+                            break;
+                case "mul": tokens.add(" * ");
+                            break;
+                case "div": tokens.add(" / ");
+                            break;
+                case "pow": tokens.add("^");
+                            break;
+                case "mod": tokens.add(" % ");
+                            break;
+                default:
+                    if (isOperand(s))
+                        tokens.add(s);
+                    else
+                        error();
+                    break;
+            }
+        }
+        return tokens;
+    }
+
+    public void error(){
+        System.out.println("Syntax error");
+        System.exit(-1);
+    }
+
+    public Boolean isOperand(String op){
+        return (op.matches("[0-9]+"));
+    }
+
 }
--- ExpressionTransformationProject/src/Main.java
+++ ExpressionTransformationProject/src/Main.java
@@ -1,12 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
+        Lexer lexer = new Lexer();
+
         try(Scanner sc = new Scanner(new File("test.txt"))){
+            List<String> tokens;
+            while(sc.hasNextLine())
+            {
+                tokens = lexer.tokenizer(sc.nextLine());
+                System.out.println(tokens);
+            }
+
+
 
         }catch (NoSuchElementException e){
             e.printStackTrace();
